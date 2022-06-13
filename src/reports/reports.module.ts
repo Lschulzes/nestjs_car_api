043813058e4c2d6c60080e09aaedb16d4df77687
lschulzes <lucassilvaiee@gmail.com>
2022-06-13 18:52:29 -0300

    feat: add report entity

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { Report } from './report.entity';
 
 @Module({
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
+  imports: [TypeOrmModule.forFeature([Report])],
 })
 export class ReportsModule {}
