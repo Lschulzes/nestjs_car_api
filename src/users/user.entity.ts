2b868629720a7f74319d0d9a82a5db7758e2546b
lschulzes <lucassilvaiee@gmail.com>
2022-06-13 22:24:51 -0300

    v0: create user

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,38 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail, MinLength } from 'class-validator';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
+  @IsEmail()
   email: string;
 
   @Column()
+  @MinLength(8)
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log(`User inserted with the id of ${this.id}`);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`User with the id of ${this.id} has been updated`);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log(`User with the id of ${this.id} has been deleted`);
+  }
 }
