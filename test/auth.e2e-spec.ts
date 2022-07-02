b9c5c0764342a4ce42102be0e0c276b992138ce4
lschulzes <lucassilvaiee@gmail.com>
2022-07-02 19:53:25 -0300

    test: add e2e on auth service

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -14,10 +14,11 @@ describe('AuthSys (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
     await app.init();
   });
 
-  it('handles a signup request', () => {
+  it('handles a signup request', async () => {
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password })
@@ -28,5 +29,21 @@ describe('AuthSys (e2e)', () => {
         expect(id).toBeDefined();
         expect(returnedEmail).toEqual(email);
       });
+  });
+
+  it('signup as a new user and then get the currently logged user', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/me')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
