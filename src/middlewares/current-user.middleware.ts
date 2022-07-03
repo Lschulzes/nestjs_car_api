83860b618c629391d62cb845f62767d77a9de751
lschulzes <lucassilvaiee@gmail.com>
2022-07-03 11:53:45 -0300

    chore: extend Express Request to have user

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -1,7 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { UserSession } from 'src/users/dto/user-input';
+import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -13,7 +22,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
     if (userId) {
       const user = await this.usersService.findOne(userId);
 
-      req['user'] = user;
+      req.user = user;
     }
 
     next();
